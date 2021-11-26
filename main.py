89f7634d863009a9defce07319ce49d558c987c2
PratikANaik <pratik_an@protonmail.com>
2021-11-26 17:46:41 +0100

    Foreground Extraction with chromakey works well

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ Generator using Image Compositions
 import functions.foreground_extraction as fg_ex
 
 def main():
-    dir_path = '/home/pratikan/Synthetic_Image_Generation/SIG/Data/Classes'
+    dir_path = './Data/Classes'
     ck = fg_ex.FgExtractor(dir_path,
             'ChromaKey', 'False')
-    ck.print_settings()
+    print(ck.print_settings())
+    ck.prepare_folders()
     ck.extract_foregrounds()
 
 if __name__ == '__main__':
