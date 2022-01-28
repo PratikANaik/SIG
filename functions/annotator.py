e1751516f80fa4094814d181485e2eefd3680210
PratikANaik <pratik_an@protonmail.com>
2022-01-28 20:05:58 +0100

    finishes annotator function

--- functions/annotator.py
+++ functions/annotator.py
@@ -4,4 +4,115 @@ compositions in the COCO format using
 Pycococreatortools.
 """
 import json
+import numpy as np
 import os
+from pycococreatortools import pycococreatortools
+import datetime
+import fnmatch
+import re
+from tqdm import tqdm
+from PIL import Image
+
+INFO = {
+    "description": "Example Dataset",
+    "url": "https://github.com/PratikANaik",
+    "version": "0.1.0",
+    "year": 2021,
+    "contributor": "Pratik",
+    "date_created": datetime.datetime.utcnow().isoformat(' ')
+}
+
+LICENSES = [
+    {
+        "id": 1,
+        "name": "Attribution-NonCommercial-ShareAlike License",
+        "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+    }
+]
+
+CATEGORIES = [
+    {"supercategory": "food","id": 1,"name": "pizza"},
+    {"supercategory": "sport","id": 2,"name": "racket"},
+    {"supercategory": "human","id": 3,"name": "person"},
+]
+
+def filter_for_jpeg(root, files):
+    file_types = ['*.jpeg', '*.jpg']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(file_types, f)]
+    
+    return files
+
+def filter_for_annotations(root, files, image_filename):
+    file_types = ['*.png']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    basename_no_extension = os.path.splitext(os.path.basename(image_filename))[0]
+    file_name_prefix = basename_no_extension + '_.*'
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(file_types, f)]
+    files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f).lower())[0])]
+
+    return files
+
+def annotate(image_dir, annotation_dir,
+            dataset_name=datetime.datetime.utcnow().isoformat(' ')):
+
+    coco_output = {
+        "info": INFO,
+        "licenses": LICENSES,
+        "categories": CATEGORIES,
+        "images":[],
+        "annotations":[]
+    }
+
+    image_id = 1
+    segmentation_id = 1
+
+    # Filter for jpeg images
+    for root, _, files in os.walk(image_dir):
+        image_files = filter_for_jpeg(root, files)
+
+        # go through each image
+        for image_filename in (tqdm(image_files)):
+            image = Image.open(image_filename)
+            image_info = pycococreatortools.create_image_info(
+                        image_id,
+                        os.path.basename(image_filename),
+                        image.size)
+            coco_output["images"].append(image_info)
+
+            # filter for associated png annotations
+            for root, _, files in os.walk(annotation_dir):
+                annotation_files = filter_for_annotations(root,
+                                files, image_filename)
+                
+                # go through each annotation
+                for annotation_filename in annotation_files:
+                    class_id = [x['id'] for x in CATEGORIES
+                                if x['name'] in annotation_filename][0]
+                    
+                    category_info = {'id':class_id,
+                                'is_crowd':'crowd' in image_filename}
+                    
+                    binary_mask = np.asarray(
+                            Image.open(annotation_filename
+                            ).convert('1')
+                            ).astype(np.uint8)
+                    
+                    annotation_info = pycococreatortools.create_annotation_info(
+                            segmentation_id, image_id,
+                            category_info, image.size,
+                            tolerance=2)
+
+                    if annotation_info is not None:
+                        coco_output["annotations"].append(annotation_info)
+
+                    segmentation_id += 1
+
+            image_id += 1
+
+        json_path = f'./Output/instances_{dataset_name}.json'
+        with open(json_path) as output_json:
+            json.dump(coco_output, output_json)
+                
