c9dbe6bb8d0f2874741c1deb2442e24f92da8a12
PratikANaik <pratik_an@protonmail.com>
2022-01-31 13:03:05 +0100

    Example main file for composing and annotating synthetic images

--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 Main file for the CLI for Synthetic Image
 Generator using Image Compositions
 """
+import numbers
 import functions.foreground_extraction as fg_ex
+import functions.image_composer as im_co
+import functions.annotator as annot
+import os
 
 def main():
-    dir_path = './Data/Classes'
-    ck = fg_ex.FgExtractor(dir_path,
-            'U2Net', 'False')
-    print(ck.print_settings())
-    ck.prepare_folders()
-    ck.extract_foregrounds()
+    print(os.getcwd())
+    res = (640,480)
+    loc = ['Racket', 'Person', 'Pizza']
+    ImgComp = im_co.ImgComposer(resolution=res,
+                            classes_to_include=loc,
+                            num_of_images=2,
+                            max_objects_in_image=4,
+                            output_directory='./Output/Composed',
+                            efo_directory='./Output/EFObjects')
+    ImgComp.compose()
+    annot.annotate(image_dir='./Output/Composed',
+        annotation_dir='./Output/Annotations',
+        dataset_name='test')
 
 if __name__ == '__main__':
     main()
